8892220206d4a09e705e128f3c900aa65075fd83
tunip9410 <tunip943@gmail.com>
2022-01-17 03:34:47 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from "three"
+import {Canvas, useFrame} from "@react-three/fiber"
+import {OrbitControls, Stars} from "@react-three/drei"
+
+function Box() {
+    return(
+        <mesh position={[0, 2, 0]}>
+            <boxBufferGeometry attach="geometry"></boxBufferGeometry>
+            <meshLambertMaterial attach="material" color="green"/>
+        </mesh>
+    )
+}
+
+function Plane() {
+    return(
+        <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeBufferGeometry attach="geometry" args={[10, 10]}/>
+            <meshLambertMaterial attach="material" color="blue"/>
+        </mesh>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Canvas>
+                <OrbitControls addEventListener={undefined} hasEventListener={undefined} removeEventListener={undefined} dispatchEvent={undefined}/>
+                <Stars/>
+                <ambientLight intensity={0.8}/>
+                <spotLight position={[10, 15, 10]} angle={0.3}/>
+                <Box/>
+                <Plane/>
+            </Canvas>
+        </div>
+    );
 }
 
 export default App;
